5be542ba9e726cdbef96c328e3af82f500f16904
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2021-12-18 16:06:39 -0700

    question validation

--- app.js
+++ app.js
@@ -14,12 +14,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?',
+      message: 'What is your name? (Required)',
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name');
+          return false;
+        }
+      },
     },
     {
       type: 'input',
       name: 'github',
       message: 'Enter your GitHub Username',
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log('Please enter your github username');
+          return false;
+        }
+      },
     },
     {
       type: 'input',
@@ -44,11 +60,27 @@ const promptProject = (portfolioData) => {
         type: 'input',
         name: 'name',
         message: 'What is the name of your project?',
+        validate: (projectInput) => {
+          if (projectInput) {
+            return true;
+          } else {
+            console.log('Please enter your project name');
+            return false;
+          }
+        },
       },
       {
         type: 'input',
         name: 'description',
         message: 'Provide a description of the project (Required)',
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter your project description');
+            return false;
+          }
+        },
       },
       {
         type: 'checkbox',
@@ -68,6 +100,14 @@ const promptProject = (portfolioData) => {
         type: 'input',
         name: 'link',
         message: 'Enter the GitHub link to your project. (Required)',
+        validate: (linkInput) => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log('Please enter your github link');
+            return false;
+          }
+        },
       },
       {
         type: 'confirm',
